c0ea79cc534c16fdaf3d3b072f423f16467aa56b
Rubenia Borge <rubeniaolaska@gmail.com>
2022-09-29 00:33:27 -0500

    Update lesson_1.py

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,35 @@
 
+# Introduction to Python
+
+# Print statemtents 
+
+print('Hello CSCE 1010 UNT!')
+
+'''
+Arithmetic and Variables
+
+Addition +
+Substraction -
+Multiplication *
+Division /
+Exponent **
+
+'''
+
+# Example of Arithmetic Operators
+print( 1 + 2)
+print (5 - 4)
+print (2 * 4)
+print(6 / 3)
+print (3**2)
+
+# Control the order of arithmetic operations using parentesis
+
+print('First value')
+print(((1 + 3) * (9 - 2) / 2) ** 2)
+
+print('Second value')
+print (1 + 3 * 9 - 2 / 2 ** 2)
+
+print('Third value')
+print ((1 + 3)* 9 - 2 / 2 ** 2)
